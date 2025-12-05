c68c5e4f11e4112ec7b5cb32764a63189f675fd4
Dalal-Alruwaili <2.dalal01@gmail.com>
2025-12-05 11:23:10 +0300

    SW4-1: Implement delete book feature by index

--- src/Library.java
+++ src/Library.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-
 public class Library {
     private ArrayList<Book> books = new ArrayList<>();
 
@@ -7,7 +6,32 @@ public class Library {
         books.add(new Book(title, author, isbn));
         System.out.println("Book added successfully.");
     }
+    public void deleteBook(int index) {
+        if (books.isEmpty()) {
+            System.out.println("No books available to delete.");
+            return;
+        }
 
+        if (index >= 0 && index < books.size()) {
+            System.out.println("\nDeleting: " + books.get(index));
+            books.remove(index);
+            System.out.println("Book deleted successfully.");
+        } else {
+            System.out.println("Invalid index. No book deleted.");
+        }
+    }
+
+    public void showBooksWithIndex() {
+        if (books.isEmpty()) {
+            System.out.println("No books available.");
+            return;
+        }
+
+        System.out.println("\n---- Books List ----");
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println(i + ": " + books.get(i));
+        }
+    }
     public void showBooks() {
         if (books.isEmpty()) {
             System.out.println("No books available.");
@@ -15,8 +39,8 @@ public class Library {
         }
 
         System.out.println("\n---- Library Books ----");
-        for (Book book : books) {
-            System.out.println(book);
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println(i + ". " + books.get(i));
         }
     }
 }
