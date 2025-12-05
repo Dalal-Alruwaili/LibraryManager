9160afc710183964a26cf34016e0c634d4ad2e22
Dalal-Alruwaili <2.dalal01@gmail.com>
2025-12-05 13:24:31 +0300

    SW4-3: Add search functionality option in Main menu

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,10 @@ public class Main {
             System.out.println("2. Show Books");
             System.out.println("3. Edit Book");
             System.out.println("4. Delete Book");
-            System.out.println("5. Exit");
+            System.out.println("5. Search Book");
+            System.out.println("6. Exit");
             System.out.print("Choose an option: ");
-            
+
             int choice = scanner.nextInt();
             scanner.nextLine();
 
@@ -37,14 +38,14 @@ public class Main {
                     System.out.print("Enter index of the book to edit: ");
                     int editIndex = scanner.nextInt();
                     scanner.nextLine();
-                    
+
                     System.out.print("Enter New Title: ");
                     String newTitle = scanner.nextLine();
                     System.out.print("Enter New Author: ");
                     String newAuthor = scanner.nextLine();
                     System.out.print("Enter New ISBN: ");
                     String newIsbn = scanner.nextLine();
-                    
+
                     library.editBook(editIndex, newTitle, newAuthor, newIsbn);
                     break;
 
@@ -55,8 +56,12 @@ public class Main {
                     scanner.nextLine();
                     library.deleteBook(delIndex);
                     break;
-
                 case 5:
+                    System.out.print("Enter keyword to search: ");
+                    String keyword = scanner.nextLine();
+                    library.searchBook(keyword);
+                    break;
+                case 6:
                     System.out.println("Exiting program...");
                     return;
 
